a4010989c5adbf6e5692682b59efe4d463f36590
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-07-25 12:45:02 +0100

    Fixed anchor colour change on scroll

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.topnav');
+  const navs = document.querySelectorAll('.navs')
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
         navbar.classList.add('navbar-white');
+        navs.forEach((nav) => {
+          nav.classList.add('black')
+        })
       } else {
         navbar.classList.remove('navbar-white');
+        navs.forEach((nav) => {
+          nav.classList.remove('black')
+        })
       }
     });
   }
